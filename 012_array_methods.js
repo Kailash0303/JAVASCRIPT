66c0ceb15a4eddef434ee68ce58ee40b20f33d96
Kailash0303 <kailash.tak0303@gmail.com>
2025-02-09 23:20:26 +0530

    added 012_array_methods.js

---
+++ 012_array_methods.js
@@ -0,0 +1,106 @@
+// -----array methods 
+/*
+let food=["apple" ,"potato" , "banana" , "chips"];
+console.log(food);
+
+
+console.log("----- push() -----");
+
+food.push("panner")
+console.log(food);
+
+food.push("panner" , "piza" , "burger")
+console.log(food);
+
+console.log("----- pop() -----");
+let deleted= food.pop();
+console.log(food);
+console.log("deleted item :" ,deleted);
+
+console.log("----- tostring() -----");
+console.log(food.toString());
+console.log(food);
+
+
+let marks=[97,85,65,25,31,18];
+
+
+console.log("----- concat() -----");
+
+let marvel=["iron man" , "thor" , "spiderman" , "hulk"];
+let dc=["batman" , "dcman"];
+let indian=["shaktiamn" , "kirsh"];
+
+let heros=marvel+dc+indian; //one way
+console.log(heros);
+
+let heros2=marvel.concat(dc , indian)  //second way
+console.log(heros2);
+
+let marvel=["iron man" , "thor" , "spiderman" , "hulk"];
+
+
+console.log("----- unshift() -----");
+marvel.unshift("antman")
+console.log(marvel);
+
+console.log("----- shift() -----");
+let d1=marvel.shift();
+console.log(d1);
+console.log(marvel);
+
+
+let marvel=["iron man" , "thor" , "spiderman" , "hulk"];
+
+console.log(marvel);
+console.log("----- slice() -----");
+console.log(marvel.slice(0,3));
+
+*/
+
+console.log("----- splice() -----");
+
+let arr=[1,2,3,4,5,6,7];
+console.log(arr);
+
+// arr.splice(2 , 2 , 101 , 102 );
+// console.log(arr);
+
+/*
+//add element
+arr.splice(2 , 0 ,101)
+console.log(arr);
+
+//delete element
+arr.splice(3 , 1 )
+console.log(arr);
+
+//replace element
+arr.splice(2 , 1 ,505)
+console.log(arr);
+*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
