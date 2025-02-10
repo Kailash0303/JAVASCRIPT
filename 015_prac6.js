6abc6f6d169c4097ecfd3619c1d51e2ba4e806b2
Kailash0303 <kailash.tak0303@gmail.com>
2025-02-10 18:01:44 +0530

    modified 015_prac6.js

--- 015_prac6.js
+++ 015_prac6.js
@@ -1,23 +1,77 @@
 //practise set 6
 
-function vowelsfun(str){
+
+//Q1
+function vowelcount(str){
     
-    let v=0;
+    let count=0;
     let str2 = str.toLowerCase();
-    console.log(str2);
-    
+
     for(let i =0 ; i<str.length ; i++){
-    //    console.log(str[i]);
-       
-        if(str[i] == "a"|| str[i] == "e" || str[i] == "i" || str[i] == "o" || str[i] == "u" ){
-            v++;
+
+        if(str2[i] == "a"|| str2[i] == "e" || str2[i] == "i" || str2[i] == "o" || str2[i] == "u" ){
+            count++;
         }
     }
-    // console.log(v);
     
-    return v;
+    return count;
 }
-console.log(vowelsfun("kailash"));
+console.log(vowelcount("AEIOU"));
 
-   
+//Q2
 
+let arrowcount =(str)=>{
+
+    let count=0;
+    let str2 = str.toLowerCase();
+    for(let i =0 ; i<str.length ; i++){
+
+        if(str2[i] == "a"|| str2[i] == "e" || str2[i] == "i" || str2[i] == "o" || str2[i] == "u" ){
+            count++;
+        }
+    }
+    return count;
+}
+
+console.log(arrowcount("kaialsh"));
+
+
+
+
+
+
+
+
+
+
+
+// //Q1
+// function vowelcount(str){
+    
+//     let count=0;
+//     let str2 = str.toLowerCase();
+//     // console.log(str2);
+
+//     /* using for of loop
+//     for(let char of str2){
+//         if(char === "a"|| char === "e" || char === "i" || char === "o" || char === "u" ){
+//             count++; 
+//     }
+//     }*/
+
+
+//     /*
+//     //using for loop
+//     for(let i =0 ; i<str.length ; i++){
+
+//         if(str2[i] == "a"|| str2[i] == "e" || str2[i] == "i" || str2[i] == "o" || str2[i] == "u" ){
+//             count++;
+//         }
+//     }*/
+//     // console.log(v);
+    
+//     return count;
+// }
+// console.log(vowelcount("AEIOU"));
+
+// 
