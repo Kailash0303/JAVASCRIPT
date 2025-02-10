442f797e13c0580daae43dfe89fb86d0c6e86c37
Kailash0303 <kailash.tak0303@gmail.com>
2025-02-10 23:51:08 +0530

    added 016_foreach.js

---
+++ 016_foreach.js
@@ -0,0 +1,20 @@
+let arr=[1,2,3,4,5];
+
+
+//func as parameter to func
+arr.forEach(function printval(val){
+    console.log(val); 
+});
+
+// using arrowfunctions
+console.log("---- using arrowfunctions ----");
+let arr2=["pune" , "mumbai" , "delhi"]
+arr2.forEach((val) => {
+    console.log(val.toUpperCase());
+})
+
+console.log("---- using arrowfunctions PART 2 ----");
+let arr3=["pune" , "mumbai" , "delhi"]
+arr3.forEach((val , idx , arr) => {
+    console.log(val.toUpperCase() , idx , arr);
+})
