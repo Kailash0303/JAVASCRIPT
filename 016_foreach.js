e8930c269a771a7594665f17540c59e7f3327bb4
Kailash0303 <kailash.tak0303@gmail.com>
2025-02-11 00:00:09 +0530

    modified 016_foreach.js

--- 016_foreach.js
+++ 016_foreach.js
@@ -1,3 +1,4 @@
+/*
 let arr=[1,2,3,4,5];
 
 
@@ -18,3 +19,24 @@ let arr3=["pune" , "mumbai" , "delhi"]
 arr3.forEach((val , idx , arr) => {
     console.log(val.toUpperCase() , idx , arr);
 })
+
+
+*/
+//practise question 
+
+let array=[1,2,3,4,5,6];
+/*
+//one way
+array.forEach((val) =>{
+    console.log(val*val);
+});
+*/
+
+//second way
+const calSquare = (val) =>{
+    console.log(val*val);
+};
+
+array.forEach(calSquare);
+
+
