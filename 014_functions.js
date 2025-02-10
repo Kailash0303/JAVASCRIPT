f9251ab881a7f148a0a92823e349a1f86ffcf240
Kailash0303 <kailash.tak0303@gmail.com>
2025-02-10 10:31:15 +0530

    014_functions.js

---
+++ 014_functions.js
@@ -0,0 +1,50 @@
+//function in js
+
+/*
+
+//function_defination
+function myFunc(){
+    console.log("welcome to js");
+    console.log("happt learning js :)");
+}
+
+//function_call
+myFunc();
+
+function myFunc2(msg , n){         //parameter  ---> msg
+    console.log(msg + n);
+}
+
+myFunc2("i love js ",10);             //arguments ---> i love js
+
+//func to add 2 numbers
+function sum(x , y) {
+    console.log(x+y);
+}
+
+function sum2(x , y) {
+    
+    s=x+y;
+    console.log("before return");
+    return s;
+    console.log("after return");   // unreachable code 
+}
+
+let val= sum2(10,50);
+console.log(val);
+
+// console.log(x);  //error in console
+
+
+// part of modern java
+const arrowsum = (a,b) =>{
+    console.log(a+b);
+ }
+
+let arrowmul =(x,y) =>{
+    console.log(x*y);
+}
+
+*/
+ const print =() => console.log("hello");
+ 
