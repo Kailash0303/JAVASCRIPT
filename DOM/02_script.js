df7dcdc2be4844d7f06b3ff53ec7560b07aab044
Kailash0303 <kailash.tak0303@gmail.com>
2025-02-12 13:25:39 +0530

    added 02_script.js

---
+++ DOM/02_script.js
@@ -0,0 +1,53 @@
+/*
+console.log("hello");
+
+let a=document.getElementById("123");
+console.log(a);
+console.dir(a);
+
+let b=document.getElementById("456");
+console.log(b);
+console.dir(b);
+
+let c=document.getElementsByClassName("h-class")
+console.log(c);
+console.dir(c);
+
+let d=document.getElementById("myid");
+console.log(d);
+console.dir(d);
+
+let e = document.getElementsByTagName("p")    
+console.log(e);
+console.dir(e);
+
+
+
+//query selector
+console.log("--- query selector ----");
+
+console.log("--- query selector for tag----");
+fele=document.querySelector("p");
+console.log(fele);
+console.dir(fele);
+
+allele=document.querySelectorAll("p");
+console.log(allele);
+console.dir(allele);
+
+
+
+console.log("--- query selector for class ----");
+
+firstclass=document.querySelector(".h-class");
+console.dir(firstclass);
+
+allclass=document.querySelectorAll(".h-class");
+console.dir(allclass);
+
+*/  
+
+console.log("--- query selector for id ----");
+
+firstid = document.querySelector("#myid");
+console.dir(firstid);
