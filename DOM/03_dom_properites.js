1557876e8e48f4aaf09d1ba274e30f1d509bdab6
Kailash0303 <kailash.tak0303@gmail.com>
2025-02-12 17:23:17 +0530

    added 03_dom_properites.js

---
+++ DOM/03_dom_properites.js
@@ -0,0 +1,17 @@
+/*
+//tagName
+console.log("--- tagName-----");
+
+firstid = document.querySelector("#myid");
+console.dir(firstid);
+
+console.log(firstid.tagName);
+*/
+
+let div = document.querySelector("div");
+console.log(div.innerText);
+console.log(div.innerHTML);
+
+// div.innerText="abcd"
+
+let heading = document.querySelector("h1");
