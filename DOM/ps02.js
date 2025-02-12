1e6c1b5f89abb1f68f905f00b25a3a12d4c360bf
Kailash0303 <kailash.tak0303@gmail.com>
2025-02-12 23:18:45 +0530

    completed practise set 2

---
+++ DOM/ps02.js
@@ -0,0 +1,15 @@
+/*
+let newbtn=document.createElement("button");
+newbtn.innerText="click !me";
+
+newbtn.style.backgroundColor="red";
+newbtn.style.color="white";
+
+document.querySelector("body").prepend(newbtn);
+*/
+
+let para =document.querySelector("p");
+
+console.log(para.getAttribute("class"));
+
+// para.setAttribute("class" , "newclass");
