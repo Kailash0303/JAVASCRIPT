89c0626b474b0217dc58346eaa72c478cbf7b2be
Kailash0303 <kailash.tak0303@gmail.com>
2025-02-12 13:24:18 +0530

    added

---
+++ DOM/01_script.js
@@ -0,0 +1,12 @@
+// alert("hello")
+// prompt("welcome back")
+// console.log("hello");
+// const obj = { name: "Kailash", age: 25, greet: function() { return "Hello!"; } };
+
+// console.log(obj); // Shows object in a structured format
+// console.dir(obj); // Shows an expandable list of object properties
+
+// console.log(document.body);
+// console.dir(document.body);
+// console.log(window);
+// console.dir(window);
